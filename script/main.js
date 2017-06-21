26f941045d54b8eec55d974bd1707cd0796d7507
Christopher Robus <robusc@Christopher-Robus.local>
2017-06-20 20:43:37 -0400

    adding header images

--- script/main.js
+++ script/main.js
@@ -1,7 +1,25 @@
 
-Math.floor((Math.random() * 10) + 1);
+number = Math.floor(Math.random() * 10);
+var firstSet = number
+var secondSet = number + 10
+var thirdSet = number + 20
+var fourthSet = number + 30
 
-var x = document.getElementsByClassName("image-header")[0];
 
-imageName = "static/img" + x
+
+var x = document.getElementsByClassName("image-header-one")[0];
+imageName = "static/img" + firstSet
 x.src=imageName
+
+var x = document.getElementsByClassName("image-header-two")[0];
+mageName = "static/img" + secondSet
+x.src=imageName
+
+var x = document.getElementsByClassName("image-header-three")[0];
+imageName = "static/img" + thirdSet
+x.src=imageName
+
+var x = document.getElementsByClassName("image-header-four")[0];
+imageName = "static/img" + fourthSet
+x.src=imageName
+
