ec1c19496cd8741327bd56dc6009295bed1e4b51
Chris Robus <christopher.robus@gmail.com>
2017-06-20 20:53:48 -0400

    Update main.js

--- script/main.js
+++ script/main.js
@@ -8,18 +8,18 @@ var fourthSet = number + 30
 
 
 var x = document.getElementsByClassName("image-header-one")[0];
-imageName = "static/img" + firstSet
+imageName = "static/img" + firstSet + ".JPG"
 x.src=imageName
 
 var x = document.getElementsByClassName("image-header-two")[0];
-mageName = "static/img" + secondSet
+mageName = "static/img" + secondSet + ".JPG"
 x.src=imageName
 
 var x = document.getElementsByClassName("image-header-three")[0];
-imageName = "static/img" + thirdSet
+imageName = "static/img" + thirdSet + ".JPG"
 x.src=imageName
 
 var x = document.getElementsByClassName("image-header-four")[0];
-imageName = "static/img" + fourthSet
+imageName = "static/img" + fourthSet + ".JPG"
 x.src=imageName
 
