3ae55a2b17fbb8cbc2e737a43ee0e5c465c1e0bb
Chris Robus <christopher.robus@gmail.com>
2017-06-20 20:57:00 -0400

    Update main.js

--- script/main.js
+++ script/main.js
@@ -8,18 +8,18 @@ var fourthSet = number + 30
 
 
 var x = document.getElementsByClassName("image-header-one")[0];
-imageName = "http://joerobusbachelor.party/static/img" + firstSet + ".JPG"
+imageName = "http://joerobusbachelor.party/static/IMG" + firstSet + ".JPG"
 x.src=imageName
 
 var x = document.getElementsByClassName("image-header-two")[0];
-mageName = "http://joerobusbachelor.party/static/img" + secondSet + ".JPG"
+mageName = "http://joerobusbachelor.party/static/IMG" + secondSet + ".JPG"
 x.src=imageName
 
 var x = document.getElementsByClassName("image-header-three")[0];
-imageName = "http://joerobusbachelor.party/static/img" + thirdSet + ".JPG"
+imageName = "http://joerobusbachelor.party/static/IMG" + thirdSet + ".JPG"
 x.src=imageName
 
 var x = document.getElementsByClassName("image-header-four")[0];
-imageName = "http://joerobusbachelor.party/static/img" + fourthSet + ".JPG"
+imageName = "http://joerobusbachelor.party/static/IMG" + fourthSet + ".JPG"
 x.src=imageName
 
